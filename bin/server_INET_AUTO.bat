a2a9dce0f940a09f9357c176bdbc69d73090c61b
Sean <donahue144@gmail.com>
2016-12-10 21:33:07 -0500

    Added OPTIONAL auto-restart file

---
+++ bin/server_INET_AUTO.bat
@@ -0,0 +1,36 @@
+@echo off
+title RoR_Server
+echo %time% Stating Rigs of Rods Server...
+:start
+cd ./bin
+if exist rorserver.exe ( 
+	rorserver.exe -c "../settings/server.cfg" -inet
+) else (
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo  ##############################################################################
+	echo  ##                                                                          ##
+	echo  ##                     File 'rorserver.exe' is missing.                     ##
+	echo  ##                                                                          ##
+	echo  ## Please download it from http://sourceforge.net/projects/rorserver/files/ ##
+	echo  ##            and extract the archive inside the 'bin' directory.           ##
+	echo  ##                                                                          ##
+	echo  ##############################################################################
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+	echo.
+)
+cd ..
+goto start
+pause
